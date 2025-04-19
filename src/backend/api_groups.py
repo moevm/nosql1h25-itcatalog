2abbd66de821dfbf28d25874b58109ca19609192
MagzhanSerikov <112800073+MagzhanSerikov@users.noreply.github.com>
2025-04-19 15:55:27 +0300

    add api groups

---
+++ src/backend/api_groups.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException
+# include "main.py"
+from docker.backend.main import driver, app
+
+@app.get("/api/groups/{group_type}")
+async def get_groups(group_type: str):
+    valid_types = {"categories": "Category", "skillgroups": "SkillGroup", "technologygroups": "TechnologyGroup", "toolgroups": "ToolGroup"}
+    if group_type not in valid_types.keys():
+        raise HTTPException(status_code=400, detail="Invalid group type")
+    try:
+        with driver.session() as session:
+            result = session.run(
+                f"MATCH (g:{valid_types[group_type]}) RETURN g.name as name"
+            )
+            return [record["name"] for record in result]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
