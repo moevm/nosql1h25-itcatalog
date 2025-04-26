2cb07bff031ca6056b25f4333083063474a7790d
Medvedsky Daniil <112641584+MedvedsckyDaniil@users.noreply.github.com>
2025-04-26 15:55:49 +0300

    Update api_professions.py: add `ORDER by toLower(p.name)` to every function

--- src/backend/routes/api_professions.py
+++ src/backend/routes/api_professions.py
@@ -3,6 +3,7 @@ from database import driver
 
 router = APIRouter(prefix="/api/professions", tags=["professions"])
 
+
 @router.get("")
 async def get_professions():
     try:
@@ -60,6 +61,7 @@ async def get_profession(id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @router.get("/filter/categories")
 async def get_professions_sorted_by_categories():
     try:
@@ -68,26 +70,20 @@ async def get_professions_sorted_by_categories():
                 """
                 MATCH (p:Profession)-[:BELONGS_TO]->(c:Category)
                 RETURN p.name AS profession_name, c.name AS category_name
-                ORDER BY c.name, p.name
+                ORDER BY toLower(category_name), toLower(p.name)
                 """
             )
-            categories_professions = {}
-            for record in result:
-                category = record["category_name"]
-                profession = record["profession_name"]
-                if category not in categories_professions:
-                    categories_professions[category] = []
-                categories_professions[category].append(profession)
             return [
                 {
-                    "category": category,
-                    "professions": professions,
+                    "profession": record["profession_name"],
+                    "category": record["category_name"],
                     "image": "http://localhost:8000/static/images/in_progress.jpg"
                 }
-                for category, professions in categories_professions.items()
+                for record in result
             ]
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.get("/filter/categories/{name}")
 async def get_professions_filtered_by_category(name: str):
@@ -98,6 +94,7 @@ async def get_professions_filtered_by_category(name: str):
                 MATCH (c:Category)<-[:BELONGS_TO]-(p:Profession)
                 WHERE c.name = $name
                 RETURN p.name AS profession_name, c.name AS category_name
+                ORDER BY toLower(p.name)
                 """,
                 {"name": name}
             )
@@ -112,37 +109,6 @@ async def get_professions_filtered_by_category(name: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/filter/skills")
-async def get_professions_sorted_by_skills():
-    try:
-        with driver.session() as session:
-            result = session.run(
-                """
-                MATCH (p:Profession)-[:BELONGS_TO]->(c:Category)
-                OPTIONAL MATCH (p)-[:REQUIRES]->(s:Skill)
-                RETURN p.name AS profession_name, c.name AS category_name, s.name AS skill_name
-                ORDER BY s.name, p.name
-                """
-            )
-            categories_professions = {}
-            for record in result:
-                category = record["category_name"]
-                profession = record["profession_name"]
-                if category not in categories_professions:
-                    categories_professions[category] = set()  # Используем множество
-                categories_professions[category].add(profession)  # Добавляем без дубликатов
-            
-            # Преобразуем множества обратно в списки
-            return [
-                {
-                    "professions": list(professions),  # Конвертируем set в list
-                    "category": category,
-                    "image": "http://localhost:8000/static/images/in_progress.jpg"
-                }
-                for category, professions in categories_professions.items()
-            ]
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/filter/skills/{name}")
 async def get_professions_filtered_by_skill(name: str):
@@ -153,6 +119,7 @@ async def get_professions_filtered_by_skill(name: str):
                 MATCH (s:Skill)<-[:REQUIRES]-(p:Profession)-[:BELONGS_TO]->(c:Category)
                 WHERE s.name = $name
                 RETURN DISTINCT p.name AS profession_name, c.name AS category_name
+                ORDER BY toLower(p.name)
                 """,
                 {"name": name}
             )
@@ -167,35 +134,6 @@ async def get_professions_filtered_by_skill(name: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/filter/technologies")
-async def get_professions_sorted_by_technologies():
-    try:
-        with driver.session() as session:
-            result = session.run(
-                """
-                MATCH (p:Profession)-[:BELONGS_TO]->(c:Category)
-                OPTIONAL MATCH (p)-[:USES_TECH]->(t:Technology)
-                RETURN p.name AS profession_name, c.name AS category_name, t.name AS technology_name
-                ORDER BY t.name, p.name
-                """
-            )
-            categories_professions = {}
-            for record in result:
-                category = record["category_name"]
-                profession = record["profession_name"]
-                if category not in categories_professions:
-                    categories_professions[category] = []
-                categories_professions[category].append(profession)
-            return [
-                {
-                    "category": category,
-                    "professions": professions,
-                    "image": "http://localhost:8000/static/images/in_progress.jpg"
-                }
-                for category, professions in categories_professions.items()
-            ]
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/filter/technologies/{name}")
 async def get_professions_filtered_by_technology(name: str):
@@ -206,6 +144,7 @@ async def get_professions_filtered_by_technology(name: str):
                 MATCH (t:Technology)<-[:USES_TECH]-(p:Profession)-[:BELONGS_TO]->(c:Category)
                 WHERE t.name = $name
                 RETURN DISTINCT p.name AS profession_name, c.name AS category_name
+                ORDER BY toLower(p.name)
                 """,
                 {"name": name}
             )
@@ -220,35 +159,6 @@ async def get_professions_filtered_by_technology(name: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/filter/tools")
-async def get_professions_sorted_by_tools():
-    try:
-        with driver.session() as session:
-            result = session.run(
-                """
-                MATCH (p:Profession)-[:BELONGS_TO]->(c:Category)
-                OPTIONAL MATCH (p)-[:USES_TOOL]->(t:Tool)
-                RETURN p.name AS profession_name, c.name AS category_name, t.name AS tool_name
-                ORDER BY t.name, p.name
-                """
-            )
-            categories_professions = {}
-            for record in result:
-                category = record["category_name"]
-                profession = record["profession_name"]
-                if category not in categories_professions:
-                    categories_professions[category] = []
-                categories_professions[category].append(profession)
-            return [
-                {
-                    "category": category,
-                    "professions": professions,
-                    "image": "http://localhost:8000/static/images/in_progress.jpg"
-                }
-                for category, professions in categories_professions.items()
-            ]
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/filter/tools/{name}")
 async def get_professions_filtered_by_tool(name: str):
@@ -259,6 +169,7 @@ async def get_professions_filtered_by_tool(name: str):
                 MATCH (t:Tool)<-[:USES_TOOL]-(p:Profession)-[:BELONGS_TO]->(c:Category)
                 WHERE t.name = $name
                 RETURN DISTINCT p.name AS profession_name, c.name AS category_name
+                ORDER BY toLower(p.name)
                 """,
                 {"name": name}
             )
@@ -273,8 +184,9 @@ async def get_professions_filtered_by_tool(name: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/search/{search_term}")
-async def search_professions(search_term: str = ""):
+
+@router.get("/search/by_name/{search_term}")
+async def search_professions_by_name(search_term: str = ""):
     try:
         with driver.session() as session:
             result = session.run(
@@ -282,6 +194,7 @@ async def search_professions(search_term: str = ""):
                 MATCH (p:Profession)-[:BELONGS_TO]->(c:Category)
                 WHERE toLower(p.name) CONTAINS toLower($search_term)
                 RETURN p.name AS profession_name, c.name AS category_name
+                ORDER BY toLower(p.name)
                 """,
                 {"search_term": search_term.strip()}
             )
