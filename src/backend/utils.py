03b1959e560b09ad8239b7f271bae1a58b421402
Meshcherikov Radomir <85487164+radim601@users.noreply.github.com>
2025-04-30 18:17:56 +0300

    Update utils.py

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -17,17 +17,20 @@ def create_relationship(tx, start_node_id, end_node_id, relationship_type):
     """ % relationship_type
     tx.run(query, start_node_id=start_node_id, end_node_id=end_node_id)
     
-def check_node_exists(tx,label: str, name):
-    query = f"MATCH (p:{label}) WHERE toLower(p.name) = toLower($name) RETURN p LIMIT 1"
-    result = tx.run(query, name=name)
-    return result.single() is not None
-    
-def update_node(tx, old_name, new_label, new_properties):
+def update_node(tx, old_name, new_name, new_label, new_properties):
     query = (
         "MATCH (n) "
         "WHERE n.name=$old_name "
-        "SET n.label = $new_label, n += $new_properties "
+        "SET n.label = $new_label, n.name = $new_name, n += $new_properties "
         "RETURN n "
     )
-    tx.run(query, old_name=old_name, new_label=new_label, new_properties=new_properties)
+    tx.run(query, old_name=old_name, new_name=new_name, new_label=new_label, new_properties=new_properties)
+
+def delete_relationship(tx, start_node_id, end_node_id, rel_type):
+    query = (
+        "MATCH (a)-[r:%s]->(b) "
+        "WHERE a.id = $start_node_id AND b.id = $end_node_id "
+        "DELETE r" % rel_type
+    )
+    tx.run(query, start_node_id=start_node_id, end_node_id=end_node_id)
 
