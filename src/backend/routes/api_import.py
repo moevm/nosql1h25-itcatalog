bcd5ffbddf6afa1a99578b6abac6569724f76dc2
Meshcherikov Radomir <85487164+radim601@users.noreply.github.com>
2025-05-12 02:23:46 +0600

    fix duplicates

--- src/backend/routes/api_import.py
+++ src/backend/routes/api_import.py
@@ -23,9 +23,6 @@ async def import_data(
 
             json_candidates = []
             
-            image_extensions = {'.jpg', '.jpeg', '.png', '.webp', '.gif', '.svg'}
-            file_list = []
-            
             for root, _, files in os.walk(temp_dir):
                 if "data.json" in files:
                     json_candidates.append(os.path.join(root, "data.json"))
